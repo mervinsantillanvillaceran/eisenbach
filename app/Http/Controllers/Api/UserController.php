e1a4311210ad74cdbedaa8f7934cd6af64468d7d
Mervin Villaceran <mervinvillaceran@gmail.com>
2021-04-05 16:58:51 +0800

    users api

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['users' => User::all()]);
+    }
+
+    public function show($id)
+    {
+        return response()->json(User::find($id));
+    }
+}
